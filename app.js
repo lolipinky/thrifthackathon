1801b47bbfb87f25fd0b45d2844a69499ef704cd
pinky <ololadesamuel84@gmail.com>
2025-12-11 00:42:39 +0100

    enable credentials in CORS configuration and add group model file

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: "http://localhost:5000",
-  credentials: false,
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
