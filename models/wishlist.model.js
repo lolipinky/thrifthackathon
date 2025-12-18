79e0220cf197c827d658a9bbd6e65b399c56897e
pinky <ololadesamuel84@gmail.com>
2025-12-18 15:10:23 +0100

    added .js to my import

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -1,3 +1,7 @@
 import mongoose from 'mongoose'
 
-const wishlistSchema = new mongoose.Schema
+const wishlistSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId
+    }
+})
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 
-import { createCategory, getCategories } from "../controllers/category.controller"
+import { createCategory, getCategories } from "../controllers/category.controller.js"
 
 const categoryRouter = Router()
 categoryRouter.post('/', createCategory)
